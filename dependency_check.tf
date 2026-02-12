2367b26e24aa5345377a564f421705b8f6ae5bd0
Bartek Moczulski <12998104+bartekmo@users.noreply.github.com>
2026-02-12 13:04:10 +0100

    added dependency checks
    nic and disk type dependency checks for popular machine types for early fail if wrong types are configured.

---
+++ dependency_check.tf
@@ -0,0 +1,30 @@
+# This file is used purely to provide data for dependency checks, it does not create anything
+
+locals {
+  vm_family = split("-", var.machine_type)[0]
+  cpu_count = split("-", var.machine_type)[2]
+
+  supported_list = {
+    "c4a" = {
+      nic_types  = ["GVNIC"]
+      disk_types = ["hyperdisk-balanced", "hyperdisk-balanced-high-availability", "hyperdisk-throughput", "hyperdisk-extreme", "hyperdisk-ML"]
+    }
+    "e2" = {
+      nic_types  = ["GVNIC", "VIRTIO_NET"]
+      disk_types = ["pd-balanced", "pd-ssd", "pd-standard"]
+    }
+    "n2" = {
+      nic_types  = ["GVNIC", "VIRTIO_NET"]
+      disk_types = ["pd-standard", "pd-balanced", "pd-ssd", "pd-extreme", "hyperdisk-extreme", "hyperdisk-throughput"]
+    }
+    "c2" = {
+      nic_types  = ["GVNIC", "VIRTIO_NET"]
+      disk_types = ["pd-standard", "pd-balanced", "pd-ssd"]
+    }
+    "c4" = {
+      nic_types  = ["GVNIC"]
+      disk_types = ["hyperdisk-balanced", "hyperdisk-balanced-high-availability", "hyperdisk-throughput", "hyperdisk-extreme"]
+    }
+  }
+  supported = try(local.supported_list[local.vm_family], { "nic_types" = null, "disk_types" = null })
+}
